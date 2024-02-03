beb5e7ea55f8ab4605594f43e30d78a1f746a210
Trived <trivedhj@gmail.com>
2024-02-04 01:51:52 +0530

    feat: finished login page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { ROUTES_ARRAY } from "./constants";
+const router = createBrowserRouter(ROUTES_ARRAY);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
---
+++ src/routes/login.jsx
@@ -0,0 +1,5 @@
+import LoginScreen from "../screens/loginScreen";
+
+export default function Login() {
+  return <LoginScreen />;
+}
